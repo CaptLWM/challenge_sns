aafa6c33953931213ad085d3ca4ef82b2d58003f
CaptLWM <wonmin275@gmail.com>
2024-04-19 20:38:43 +0900

    confilct resolve

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import { redirect, useRouter } from "next/navigation";
 import Login from "./login/page";
@@ -8,12 +8,12 @@ import { useEffect } from "react";
 
 export default function Page() {
   // redirect("/i/flow/login"); // 서버에서 리다이렉트 client에서는 안먹음
-  const router = useRouter();
-  const initializing = useAuthStore((state) => state.initializing);
-  const user = useAuthStore((state) => state.user);
-  if (initializing && user) {
-    router.replace("/home"); // 로그인되지 않은 경우 로그인 페이지로 리디렉션
-  }
+  //   const router = useRouter();
+  //   const initializing = useAuthStore((state) => state.initializing);
+  //   const user = useAuthStore((state) => state.user);
+  //   if (initializing && user) {
+  //     router.replace("/home"); // 로그인되지 않은 경우 로그인 페이지로 리디렉션
+  //   }
   //   const { data: session } = useSession();
   //   if (session?.user) {
   //     // client component에서f
