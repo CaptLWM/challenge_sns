bc22fe1947e2464356cc9a5c20bfbb382f8c8831
CaptLWM <wonmin275@gmail.com>
2024-04-19 16:53:07 +0900

    회원가입 페이지 퍼블

--- src/app/(beforeLogin)/_component/signUp.tsx
+++ src/app/(beforeLogin)/_component/signUp.tsx
@@ -1,3 +1,4 @@
+import { Button, HStack, Input, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 export default function Main() {
@@ -17,5 +18,47 @@ export default function Main() {
   //       });
   //   };
 
-  return <div>signin</div>;
+  return (
+    <div className="container mx-auto mr-20 ml-20">
+      <VStack spacing={3} mb={10}>
+        <Text fontSize="6xl" as="b">
+          Just Do It
+        </Text>
+      </VStack>
+      <HStack mb={5}>
+        <Text w="10rem">아이디</Text>
+        <Input></Input>
+      </HStack>
+      <HStack mb={5}>
+        <Text w="10rem">닉네임</Text>
+        <Input></Input>
+      </HStack>
+      <HStack mb={5}>
+        <Text w="10rem">비밀번호</Text>
+        <Input></Input>
+      </HStack>
+      <HStack mb={5}>
+        <Text w="10rem">비밀번호 확인</Text>
+        <Input></Input>
+      </HStack>
+      {/* 이메일 양식 추가 필요 */}
+      <HStack mb={5}>
+        <Text w="10rem">이메일</Text>
+        <Input></Input>
+      </HStack>
+      {/* TODO 자기소개 100자이내 */}
+      <HStack mb={5}>
+        <Text w="10rem">자기소개</Text>
+        <Input></Input>
+      </HStack>
+      {/* TODO 프로필사진 */}
+      <HStack mb={10}>
+        <Text w="10rem">프로필사진</Text>
+        <Input></Input>
+      </HStack>
+      <VStack>
+        <Button>회원가입</Button>
+      </VStack>
+    </div>
+  );
 }
