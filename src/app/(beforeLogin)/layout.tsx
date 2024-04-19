e459a9dda14aaa41898c8a873a81e84fb81b393f
CaptLWM <wonmin275@gmail.com>
2024-04-19 18:06:37 +0900

    사용하지 않는 props 제거

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-type Props = { children: React.ReactNode; modal: React.ReactNode };
+type Props = { children: React.ReactNode };
 
 // 모달 사용하게 되면 추가
-export default function BeforeLoginlayout({ children, modal }: Props) {
+export default function BeforeLoginlayout({ children }: Props) {
   return (
     <div className="container mx-auto grid place-items-center content-center safe-top safe-left safe-right safe-bottom h-lvh">
       {children}
