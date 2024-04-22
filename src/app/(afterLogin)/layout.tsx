79d8ed01e6bfca32d488ebb01d71f18cd24ec362
CaptLWM <wonmin275@gmail.com>
2024-04-22 10:04:41 +0900

    nav 레이아웃 수정

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -9,22 +9,21 @@ export default function AfterLoginlayout({
 }) {
   return (
     <>
-      <div className="container flex mt-10 mx-auto safe-top safe-left safe-right safe-bottom h-lvh bg-red-200">
-        <header className="items-end flex-col flex-grow bg-orange-300">
-          <section className="w-275px h-lvh bg-orange-500">
+      <div className="container flex  mx-auto safe-top safe-left safe-right safe-bottom h-lvh bg-red-200">
+        <header className="items-end mt-10 mb-1 flex-col flex-grow-0 justify-center w-1/6 bg-orange-300">
+          <section className="flex h-lvh bg-orange-500 justify-center pt-5">
             <div className=" fixed flex flex-col items-center px-px-8 py-px-0 h-lvh">
-              <nav className="flex-1">
+              <nav className="flex-1 justify-center">
                 <ul className="list-none">
                   <NavMenu />
-                  ???
                 </ul>
               </nav>
             </div>
           </section>
         </header>
-        <div className="flex items-start flex-col flex-grow">
-          <div className="h-full flex justify-between">
-            <main>{children}</main>
+        <div className="mt-10 mb-2 flex items-start flex-col flex-grow">
+          <div className="w-full h-full flex justify-between">
+            <main className="w-full bg-lime-300">{children}</main>
           </div>
         </div>
       </div>
