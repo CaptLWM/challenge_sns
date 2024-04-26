c1dcd3db398a1dd4d429dd04ad8922e0e623387d
CaptLWM <wonmin275@gmail.com>
2024-04-27 08:17:12 +0900

    Board interface 추가

--- src/firebase/firebase.type.ts
+++ src/firebase/firebase.type.ts
@@ -1,4 +1,4 @@
-export type User = {
+export interface User {
   bio: string;
   uid: string;
   email: string;
@@ -6,4 +6,12 @@ export type User = {
   password: string;
   createdAt: string;
   updatedAt: string;
-};
+}
+
+export interface Board {
+  // challenge : string; // 챌린지 등록기능 만들고 나면 추가
+  content: string;
+  uid: string;
+  createdAt: string;
+  updatedAt: string;
+}
