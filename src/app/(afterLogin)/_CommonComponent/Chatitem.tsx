c3b1c4318684fd5aa04805c0a96232d346982076
CaptLWM <wonmin275@gmail.com>
2024-05-13 15:42:27 +0900

    1:1 채팅 수정완

--- src/app/(afterLogin)/_CommonComponent/Chatitem.tsx
+++ src/app/(afterLogin)/_CommonComponent/Chatitem.tsx
@@ -3,9 +3,15 @@ import Link from "next/link";
 import React from "react";
 
 // 채팅방 목록 하나하나
-export default function Chatitem({ nick }: { nick: string }) {
+export default function Chatitem({
+  nick,
+  roomId,
+}: {
+  nick: string;
+  roomId: string;
+}) {
   return (
-    <Link href={`/messages/${nick}`}>
+    <Link href={`/messages/${nick}?roomId=${roomId}`}>
       <Card>
         <CardBody>{nick} 채팅방</CardBody>
       </Card>
