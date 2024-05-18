b93f3e52def1c80a0c05c0a251b0d5ad8260fe8b
CaptLWM <wonmin275@gmail.com>
2024-05-18 17:07:58 +0900

    feat:팔로우하는 사람 게시물 모아보기 작업중

--- src/app/(afterLogin)/_viewComponent/home.tsx
+++ src/app/(afterLogin)/_viewComponent/home.tsx
@@ -1,37 +1,104 @@
 "use client";
 
 import { logout } from "@/firebase/firebaseAuth";
-import { Button, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BoardItemCard from "../_CommonComponent/BoardItemCard";
 import BoardCreateCard from "../_CommonComponent/BoardCreateCard";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useBoardListQuery } from "@/queries/queries";
+import { useBoardListQuery, useFollowBoardListQuery } from "@/queries/queries";
+import useAuthStore from "@/store/store";
+import { getUser } from "@/firebase/firestore";
 
 export default function Main() {
-  const boardList = useBoardListQuery();
+  const [followList, setFollowList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
 
+  const user = useAuthStore((state) => state.user);
+  const uid = user ? user.uid : null;
+
+  useEffect(() => {
+    if (uid) {
+      getUser(uid)
+        .then((data) => {
+          console.log("data", data);
+          setFollowList(data?.followingUserList);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setErr(err);
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
+  }, [uid]);
+
+  const boardList = useBoardListQuery();
+  const followBoardList = useFollowBoardListQuery(followList);
+  console.log("followingUser", followList);
+  console.log("board", boardList.data);
+  console.log("followBoard", followBoardList.data);
   return (
     <div>
       <BoardCreateCard />
-      <InfiniteScroll
-        dataLength={boardList.data?.pages.flat().length ?? 0}
-        next={boardList.fetchNextPage}
-        hasMore={boardList.hasNextPage}
-        loader={<div>Loading</div>}
-        scrollThreshold={0.8}
-      >
-        {boardList.data?.pages.map((page, pageIndex) => {
-          return (
-            <div key={pageIndex}>
-              {page.data.map((v, i) => (
-                <BoardItemCard key={v.id} props={v.data} id={v.id} />
-              ))}
-            </div>
-          );
-        })}
-      </InfiniteScroll>
+      <Tabs>
+        <TabList>
+          <Tab>전체글</Tab>
+          <Tab>팔로우</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <InfiniteScroll
+              dataLength={boardList.data?.pages.flat().length ?? 0}
+              next={boardList.fetchNextPage}
+              hasMore={boardList.hasNextPage}
+              loader={<div>Loading</div>}
+              scrollThreshold={0.8}
+            >
+              {boardList.data?.pages.map((page, pageIndex) => {
+                return (
+                  <div key={pageIndex}>
+                    {page.data.map((v, i) => (
+                      <BoardItemCard key={v.id} props={v.data} id={v.id} />
+                    ))}
+                  </div>
+                );
+              })}
+            </InfiniteScroll>
+          </TabPanel>
+          <TabPanel>
+            <Text>팔로우</Text>
+            <InfiniteScroll
+              dataLength={boardList.data?.pages.flat().length ?? 0}
+              next={boardList.fetchNextPage}
+              hasMore={boardList.hasNextPage}
+              loader={<div>Loading</div>}
+              scrollThreshold={0.8}
+            >
+              {boardList.data?.pages.map((page, pageIndex) => {
+                return (
+                  <div key={pageIndex}>
+                    {page.data.map((v, i) => (
+                      <BoardItemCard key={v.id} props={v.data} id={v.id} />
+                    ))}
+                  </div>
+                );
+              })}
+            </InfiniteScroll>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 }
